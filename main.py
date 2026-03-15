1b0a112b6f22dec288308fcdf9b24553c551b847
Ayush Mishra <aayumishra2024@gmail.com>
2026-03-15 16:23:54 +0530

    basic front end from zero

--- main.py
+++
@@ -1,5 +0,0 @@
-import os
-import eel
-eel.init("www")
-os.system(' start msedge --app="http://localhost:8000/index.html" ')   # this line will open my this project in the app format on my pc
-eel.start('index.html', mode=None , host='localhost' , block=True )
