7c02d7670801d6fb58b02e22c1a2565806a1081a
Ayush Mishra <aayumishra2024@gmail.com>
2026-03-17 01:11:32 +0530

    basic login/sign up frontend commited

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,12 +2,31 @@ let login=document.getElementById("loginBtn") ;
 let signup=document.getElementById("signupBtn") ;
 let loginform=document.getElementById("loginForm") ;
 let signupform=document.getElementById("signupForm") ;
+let closeLogin=document.getElementById("closeLogin") ;
+let closeSignup=document.getElementById("closeSignup") ;
+let logout=document.getElementById("logoutBtn") ;
 
-login.addEventListener("click",function(){
+
+/* here at the time of revising code give attention to the names of the variable of the getElementBYId */
+
+
+login.addEventListener("click",function(event){
+    event.preventDefault()
     loginform.classList.remove("hidden") ;
     signupform.classList.add("hidden") ;
 }) ;
-signup.addEventListener("click",function(){
+signup.addEventListener("click",function(event){
+    event.preventDefault()
     signupform.classList.remove("hidden") ;
     loginform.classList.add("hidden") ;
+}) ;
+closeLogin.addEventListener("click",function(){
+    loginform.classList.add("hidden") ;
+}) ;
+closeSignup.addEventListener("click",function(){
+    signupform.classList.add("hidden") ;
+}) ;
+logout.addEventListener("click",function(){
+    loginform.classList.add("hidden")
+    signupform.classList.add("hidden")
 }) ;
